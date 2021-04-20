69be2da72e1a6617512e2e79ffe3b4d5bcbdcbd9
iammrtndev <68447700+iammrtndev@users.noreply.github.com>
2021-04-20 12:13:28 +0200

    feat: downloadImage utility

---
+++ src/utils/downloadImage.ts
@@ -0,0 +1,22 @@
+import path from 'path';
+import axios from 'axios';
+import fs from 'fs';
+
+export default async function downloadImage(
+  url: string,
+  output: string
+): Promise<string> {
+  const writer = fs.createWriteStream(output);
+  const response = await axios({
+    url,
+    method: 'GET',
+    responseType: 'stream',
+  });
+  response.data.pipe(writer);
+  return new Promise((resolve, reject) => {
+    writer.on('finish', () => {
+      resolve(path.resolve(output));
+    });
+    writer.on('error', reject);
+  });
+}
