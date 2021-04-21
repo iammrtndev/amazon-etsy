2320a3adfbb01aa78e087f156dd915649db0e675
iammrtndev <68447700+iammrtndev@users.noreply.github.com>
2021-04-21 16:43:43 +0200

    feat: Amazon Scrape

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,17 @@
-import puppeteer from 'puppeteer-extra';
-import puppeteerStealth from 'puppeteer-extra-plugin-stealth';
-import { LaunchOptions } from 'puppeteer';
-import { homedir } from 'os';
+import { promptInputAsync } from './utils/promptUtils';
+import AmazonPuppeteer from './services/AmazonPuppeteer';
+import JPGImage from './services/JPGImage';
 
-puppeteer.use(puppeteerStealth());
-
-const userDir = homedir().replace(/\\/g, '/');
-
-const options: LaunchOptions = {
-  headless: false,
-  executablePath: 'C:/Program Files/Google/Chrome/Application/chrome.exe',
-  userDataDir: `${userDir}/AppData/Local/Google/Chrome/User Data`,
-  ignoreHTTPSErrors: true,
-  args: [
-    '--no-sandbox',
-    '--disable-setuid-sandbox',
-    '--disable-sync',
-    '--ignore-certificate-errors',
-    '--lang=en-US,en;q=0.9',
-  ],
-  defaultViewport: {
-    width: 1280,
-    height: 720,
-  },
-};
+const amazonScraper = new AmazonPuppeteer();
 
 (async () => {
-  const browser = await puppeteer.launch(options);
-  const page = await browser.newPage();
-  await page.goto('https://etsy.com/');
+  const input = await promptInputAsync('Amazon product url:');
+  const product = await amazonScraper.scrapeProduct(input);
+  await Promise.all(
+    product.images.map((image) => {
+      image.resize({ width: 2000 });
+      return image.saveAsync('tmp');
+    })
+  );
+  console.log(product);
 })();
