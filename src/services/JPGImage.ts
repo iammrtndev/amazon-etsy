f387a647e8e40aca99583b130c94b1af4627a835
iammrtndev <68447700+iammrtndev@users.noreply.github.com>
2021-04-21 16:31:59 +0200

    feat: JPGImage

---
+++ src/services/JPGImage.ts
@@ -0,0 +1,47 @@
+import axios from 'axios';
+import sharp from 'sharp';
+import fs from 'fs';
+import path from 'path';
+import filenamify from 'filenamify';
+
+interface resizeOptions {
+  width?: number;
+  height?: number;
+}
+
+export default class JPGImage {
+  name: string;
+  sharpOBJ: sharp.Sharp | undefined;
+  latestSavePath: string | undefined;
+
+  constructor(name: string) {
+    this.name = filenamify(name, { replacement: '' }).replace(/\s/g, '_');
+  }
+
+  async loadAsync(url: string) {
+    const response = await axios.get(url, {
+      responseType: 'arraybuffer',
+    });
+    this.sharpOBJ = sharp(response.data as Buffer);
+  }
+
+  resize(options: resizeOptions) {
+    this.sharpOBJ?.resize({
+      fit: 'contain',
+      ...options,
+    });
+  }
+
+  async saveAsync(relativePath: string) {
+    if (!fs.existsSync(relativePath)) fs.mkdirSync(relativePath);
+    const output = path.resolve(relativePath, `${this.name}.jpg`);
+    const buffer = await this.sharpOBJ?.jpeg({ mozjpeg: true }).toBuffer()!;
+    return new Promise<void>((resolve) => {
+      fs.writeFile(output, buffer, (error) => {
+        this.latestSavePath = output;
+        if (error) throw error;
+        resolve();
+      });
+    });
+  }
+}
